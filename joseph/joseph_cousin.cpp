299e751b0a95e8d1d1f1691d90b93250d2d05734
Bruno <brunohoffmann.2003@gmail.com>
2024-06-29 16:09:46 -0300

    Adiciona listagem de primos utilizados ao 'joseph_cousin.cpp'

--- joseph/joseph_cousin.cpp
+++ joseph/joseph_cousin.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+void determina_primos(int vetor[3501], int quantidade){
+    int i = 2;
+    int j = 5;
+
+    while(i < quantidade){
+        for(int k = 3; k <= sqrt(j); k += 2){
+            if(j % k == 0){
+                j += 2;
+                k = 1;
+            }
+        }
+        vetor[i] = j;
+        j += 2;
+        i += 1;
+    }
+}
+
 int main(){
+    int qtd_pessoas;
+    int seq_primos[3501] = {2, 3};
+
+    cin >> qtd_pessoas;
+    determina_primos(seq_primos, qtd_pessoas - 1);
 
     return 0;
 }    
