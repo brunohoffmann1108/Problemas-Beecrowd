766adc23f55f7bcf9b96ca51162f22d05bdd4760
Bruno <brunohoffmann.2003@gmail.com>
2024-06-29 19:47:19 -0300

    Continua resolução de joseph_cousin. primos 3 e 5 funcionando

--- joseph/joseph_cousin.cpp
+++ joseph/joseph_cousin.cpp
@@ -1,30 +1,59 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 using namespace std;
 
-void determina_primos(int vetor[3501], int quantidade){
-    int i = 2;
-    int j = 5;
+int main(){
+    int qtd_pessoas;
+    cin >> qtd_pessoas;
 
-    while(i < quantidade){
+    int pessoas[3501];
+    for(int i = 0; i < qtd_pessoas; i++){
+        pessoas[i] = i + 1;
+    }
+
+    int iterador_fixo = -1;
+    int j = 3;
+
+    if(qtd_pessoas > 1){
+        iterador_fixo += 2;
+        for(int k = iterador_fixo; k < qtd_pessoas - 1; k++){
+            pessoas[k] = pessoas[k + 1];
+        }
+        qtd_pessoas -= 1;
+        iterador_fixo -= 1;
+    }
+
+    while(1 < qtd_pessoas){
         for(int k = 3; k <= sqrt(j); k += 2){
             if(j % k == 0){
                 j += 2;
                 k = 1;
             }
         }
-        vetor[i] = j;
+        cout << iterador_fixo + 1 << endl << j << endl << qtd_pessoas << endl;
+        if(iterador_fixo + 1 + j > qtd_pessoas){
+            j -= qtd_pessoas - (iterador_fixo + 1);
+            iterador_fixo = -1;
+        }
+        cout << "J: " << j << endl;
+        iterador_fixo += j;
+        for(int k = iterador_fixo; k < qtd_pessoas - 1; k++){
+            pessoas[k] = pessoas[k + 1];
+        }
+        qtd_pessoas -= 1;
         j += 2;
-        i += 1;
+        iterador_fixo -= 1;
+
+        cout << "Pessoas" << endl;
+        for(int k = 0; k < qtd_pessoas; k++){
+            cout << pessoas[k] << endl;
+        }
+        cout << endl;       
     }
-}
 
-int main(){
-    int qtd_pessoas;
-    int seq_primos[3501] = {2, 3};
-
-    cin >> qtd_pessoas;
-    determina_primos(seq_primos, qtd_pessoas - 1);
-
+    for(int k = 0; k < qtd_pessoas; k++){
+        cout << pessoas[k] << endl;
+    }
     return 0;
 }    
