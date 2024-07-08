2db78852ee35f068801440781f3927d4c38aa131
Bruno <brunohoffmann.2003@gmail.com>
2024-07-08 20:14:04 -0300

    Adiciona restante da resolução do 'short_story_competition.cpp'

--- short_story_competition.cpp
+++ short_story_competition.cpp
@@ -1,3 +1,5 @@
+// Este código resolve o problema 1222 do Beecrowd
+
 #include <iostream>
 #include <string>
 #include <limits>
@@ -18,30 +20,36 @@ int main(){
         int linhas = 0;
 
         while(historia.size() > 0){
-            if(historia.size() <= (unsigned)char_plinha){
+
+            // Se quantidade de caracteres na string < quantidade máxima numa linha.
+            if(historia.size() <= (unsigned)char_plinha){ 
                 historia.erase(0, historia.size());
-                cout << historia << endl;
             }
-            else if(historia[char_plinha - 1] == ' '){
+
+            // Se caractere máximo for espaço.
+            else if(historia[char_plinha - 1] == ' '){ 
                 historia.erase(0, char_plinha);
-                cout << historia << endl;
             }
-            else if(historia[char_plinha] == ' '){
+
+            // Se um caractere após máximo for espaço.
+            else if(historia[char_plinha] == ' '){ 
                 historia.erase(0, char_plinha + 1);
-                cout << historia << endl;
             }
-            else{
+
+            // Se a palavra começar e não terminar (Conta apenas até a palavra anterior).
+            else{ 
                 int i = char_plinha - 1;
                 while(historia[i] != ' '){
                     i -= 1;
                 }
                 historia.erase(0, i + 1);
-                cout << historia << endl;
             }
             
             linhas += 1;
         }
-        cout << linhas << endl;
+        int paginas = linhas / linhas_ppg;
+        if(linhas % linhas_ppg > 0) paginas += 1;
+        cout << paginas << endl;
     }
 
     return 0;
