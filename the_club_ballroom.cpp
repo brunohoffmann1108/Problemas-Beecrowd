240c48c9bfbe07d994bea2c1a5df62230fc62bc2
Bruno <brunohoffmann.2003@gmail.com>
2024-07-18 18:15:36 -0300

    Adiciona fim de resolução da club-ballroom, aceita no beecrowd

--- the_club_ballroom.cpp
+++ the_club_ballroom.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int guarda_tabuas[10000];
-
-int calcula_minimo(int minimo_tabuas, int comprimento, int tabuas[10000], int tamanho_array){
+int calcula_minimo(int minimo_tabuas, int comprimento, int tabuas[100000], int tamanho_array, int guarda_tabuas[10000]){
     int i = 0;
     int total_tabuas = 0;
     int preenchidos = 0;
     while(i < tamanho_array && preenchidos < minimo_tabuas){
+        if(comprimento - tabuas[i] == tabuas[i] && guarda_tabuas[tabuas[i] - 1] < 2){
+            i += 1;
+            continue;
+        }
         if(tabuas[i] == comprimento){
             preenchidos += 1;
             total_tabuas += 1;
-            tabuas[i] = 0;
+            guarda_tabuas[tabuas[i] - 1] -= 1;
         } 
-        else if(guarda_tabuas[comprimento - (tabuas[i] + 1)] > 0){
+        else if(tabuas[i] < comprimento && guarda_tabuas[comprimento - tabuas[i] - 1] > 0 && guarda_tabuas[tabuas[i] - 1] > 0){
+            guarda_tabuas[tabuas[i] - 1] -= 1;
+            guarda_tabuas[comprimento - tabuas[i] - 1] -= 1;
             preenchidos += 1;
             total_tabuas += 2;
-            guarda_tabuas[tabuas[i - 1]] -= 1;
-            guarda_tabuas[comprimento - (tabuas[i] + 1)] -= 1;
         }
         i += 1;
     }
@@ -40,37 +42,28 @@ int main(){
         int l, k;
         cin >> l >> k;
 
-        int tabuasN[100000];
-        int tabuasM[100000];
-        int itN = 0;
-        int itM = 0;
+        int tabuas[100000] = {0};
+        int tabuasN[10000] = {0};
+        int tabuasM[10000] = {0};
         for(int i = 0; i < k; i++){ 
-            int temp;
-            cin >> temp;
-            guarda_tabuas[temp - 1] = temp;
-            if(temp <= n){
-                tabuasM[itM] = temp;
-                itM += 1;
-            } 
-            if(temp <= m){
-                tabuasN[itN] = temp;
-                itN += 1;
-            }
+            cin >> tabuas[i];
+            tabuasN[tabuas[i] - 1] += 1;
+            tabuasM[tabuas[i] - 1] += 1;
         }
 
         int minimoN = 100001;
         if((n * 100) % l == 0 && n * 100 / l <= k){
             int minimo_tabuas = n * 100 / l;
-            minimoN = calcula_minimo(minimo_tabuas, m, tabuasN, itN);
+            minimoN = calcula_minimo(minimo_tabuas, m,  tabuas, k, tabuasN);
         }
         
         int minimoM = 100001;
         if((m * 100) % l == 0 && m * 100 / l <= k){
             int minimo_tabuas = m * 100 / l;
-            minimoM = calcula_minimo(minimo_tabuas, n, tabuasM, itM);
+            minimoM = calcula_minimo(minimo_tabuas, n, tabuas, k, tabuasM);
         }
 
-        if(minimoM < minimoN) cout << minimoM << endl;
+        if(minimoM <= minimoN && minimoM != 100001) cout << minimoM << endl;
         else if(minimoN < minimoM) cout << minimoN << endl;
         else cout << "impossivel" << endl;
     }
