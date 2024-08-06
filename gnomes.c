7eb60a281c15e066b357dff5f1194e468c29f213
Bruno <brunohoffmann.2003@gmail.com>
2024-08-06 16:39:00 -0300

    Adiciona parte da resolução de gnomes, faltando ordenar idades iguais por ordem alfabética

--- gnomes.c
+++ gnomes.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct gnomo{
+    char nome[20];
+    int idade;
+} gnomo;
+
+void insertion_sort(gnomo lista_gnomos[], int num_gnomos){
+    for(int k = 1; k < num_gnomos; k++){
+        gnomo chave_struct = lista_gnomos[k];
+        int i = k - 1;
+        while(lista_gnomos[i].idade < chave_struct.idade && i > -1){
+            lista_gnomos[i + 1] = lista_gnomos[i];
+            i -= 1;
+        }
+        lista_gnomos[i + 1] = chave_struct;
+    }
+}
+
 
 int main(){
+    int num_gnomos;
+    scanf("%i", &num_gnomos);
 
+    gnomo* lista_gnomos;
+    lista_gnomos = (gnomo*)malloc(num_gnomos * sizeof(gnomo));
+    for(int i = 0; i < num_gnomos; i++){
+        scanf("%s", lista_gnomos[i].nome);
+        scanf("%i", &lista_gnomos[i].idade);
+    }    
+
+    insertion_sort(lista_gnomos, num_gnomos);
+
+    int num_times = num_gnomos / 3;
+    for(int i = 0; i < num_times; i++){
+        printf("Time %i\n", i + 1);
+        for(int j =  i; j < num_gnomos; j += num_times){
+            printf("%s %i\n", lista_gnomos[j].nome, lista_gnomos[j].idade);    
+        }
+        printf("\n");
+    }
 
     return 0;
 }
