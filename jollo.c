9a125914259f8ae9fec554613d5b7ec165bcf3e0
Bruno <brunohoffmann.2003@gmail.com>
2024-08-13 17:02:33 -0300

    Adiciona restanteda resolução de jollo mas dando erro de time limit

--- jollo.c
+++ jollo.c
@@ -10,14 +10,54 @@ void insertion_sort(int princesa[3]){
         }
         princesa[j + 1] = chave;
     }
-
-    for(int i = 0; i < 3; i++){
-        printf("%i ", princesa[i]);
-    }
 }
 
-int compara_cartas(int princesa[3], int principe[2]){
-    int vitorias_princesa = 0;
+int checa_minimo(int carta_checada, int cartas[5]){
+    carta_checada += 1;
+    int i = 0;
+    while(i < 5){
+        if(carta_checada == 53) return -1;
+        if(carta_checada == *(cartas + i)){
+            carta_checada += 1;
+            i = 0;
+            continue;
+        }
+        i += 1;
+    }
+    return carta_checada;
+}
+
+int compara_cartas(int princesa[3], int principe[2], int cartas[5]){
+    int vitorias = 0;
+    int n = 3;
+    for(int i = 1; i > -1; i--){
+        int j;
+        for(j = 0; j < n; j++){
+            if(princesa[j] > principe[i]){
+                vitorias += 1;
+                break;
+            }
+        }
+        if(j == n) j = 0;
+        for(int k = j; k < n - 1; k++){
+            princesa[k] = princesa[k + 1];
+        }
+        n -= 1;
+    }
+    if(vitorias == 2) return -1;
+    if(vitorias == 0){
+        int checada = 1;
+        int i = 0;
+        while(i < 5){
+            if(checada == *(cartas + i)){
+                checada += 1;
+                i = 0;
+                continue;
+            }
+        }
+        return checada;
+    }
+    return checa_minimo(princesa[0], cartas);
 
 }
 
@@ -25,10 +65,19 @@ int main(){
     int princesa[3];
     int principe[2];
     while(1){
-        scanf("%i %i %i %i %i", &princesa[0], &princesa[1], &princesa[2], &principe[0], &principe[1]);
-        if(princesa[0] == 0){
-            break;
+        int cartas[5];
+        int iterador_cartas = 0;
+        for(int i = 0; i < 3; i++){
+            scanf("%i", &princesa[i]);
+            cartas[iterador_cartas] = princesa[i];
+            iterador_cartas += 1;
         }
+        for(int i = 0; i < 2; i++){
+            scanf("%i", &principe[i]);
+            cartas[iterador_cartas] = principe[i];
+            iterador_cartas += 1;
+        }
+        if(princesa[0] == 0) break;
         // Ordena vetores.
         if(principe[0] > principe[1]){
             int temp = principe[0];
@@ -36,7 +85,8 @@ int main(){
             principe[1] = temp;
         }
         insertion_sort(princesa);
-
+        // Compara cartas e retorna resultado.
+        printf("%i\n", compara_cartas(princesa, principe, cartas));
     }
 
     return 0;
