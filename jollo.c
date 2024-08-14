3fdcabefee5a15f544b5a3126377a3ba81a5e5cb
Bruno <brunohoffmann.2003@gmail.com>
2024-08-14 17:12:02 -0300

    Adiciona resolução de jollo.c refeita do início

--- jollo.c
+++ jollo.c
@@ -1,92 +1,110 @@
+// Este código resolve o problema 1321 do Beecrowd.
 #include <stdio.h>
 
-void insertion_sort(int princesa[3]){
+void ordena_princesa(int princesa[]){
     for(int i = 1; i < 3; i++){
-        int chave = princesa[i];
-        int j = i - 1;
-        while(j > -1 && princesa[j] > chave){
-            princesa[j + 1] = princesa[j];
+        int j = i;
+        while(princesa[j] < princesa[j - 1] && j > 0){
+            int temp = princesa[j - 1];
+            princesa[j - 1] = princesa[j];
+            princesa[j] = temp;
             j -= 1;
         }
-        princesa[j + 1] = chave;
     }
 }
 
-int checa_minimo(int carta_checada, int cartas[5]){
-    carta_checada += 1;
-    int i = 0;
-    while(i < 5){
-        if(carta_checada == 53) return -1;
-        if(carta_checada == *(cartas + i)){
-            carta_checada += 1;
-            i = 0;
-            continue;
-        }
-        i += 1;
-    }
-    return carta_checada;
-}
-
-int compara_cartas(int princesa[3], int principe[2], int cartas[5]){
+int vitorias_princesa(int princesa[], int principe[]){
     int vitorias = 0;
-    int n = 3;
     for(int i = 1; i > -1; i--){
         int j;
-        for(j = 0; j < n; j++){
+        for(j = 0; j < 3; j++){
             if(princesa[j] > principe[i]){
+                princesa[j] = 0;
                 vitorias += 1;
                 break;
             }
         }
-        if(j == n) j = 0;
-        for(int k = j; k < n - 1; k++){
-            princesa[k] = princesa[k + 1];
-        }
-        n -= 1;
-    }
-    if(vitorias == 2) return -1;
-    if(vitorias == 0){
-        int checada = 1;
-        int i = 0;
-        while(i < 5){
-            if(checada == *(cartas + i)){
-                checada += 1;
-                i = 0;
-                continue;
+        if(j == 3){
+            for(int k = 0; k < 3; k++){
+                if(princesa[k] != 0){
+                    princesa[k] = 0;
+                    break;
+                }
             }
         }
-        return checada;
     }
-    return checa_minimo(princesa[0], cartas);
+    for(int i = 0; i < 3; i++){
+        if(princesa[i] != 0){
+            princesa[0] = princesa[i];
+        }
+    }
 
+    return vitorias;
+}
+
+int calcula_resultado(int vitorias, int cartas[], int carta_princesa){
+    switch(vitorias){
+        case 0: {
+            int i = 0;
+            int menor_carta = 1;
+            while(i < 5){
+                if(menor_carta == cartas[i]){
+                    menor_carta += 1;
+                    i = -1;
+                }
+                i += 1;
+            }
+            return menor_carta;
+        }
+        case 1: {
+            int i = 0;
+            int menor_carta = carta_princesa + 1;
+            while(i < 5){
+                if(menor_carta == 53){
+                    return -1;
+                }
+                if(menor_carta == cartas[i]){
+                    menor_carta += 1;
+                    i = -1;
+                }
+                i += 1;
+            }
+            return menor_carta;
+        }
+        case 2: {
+            return -1;
+        }
+    }
+
+    return 0;
 }
 
 int main(){
     int princesa[3];
     int principe[2];
     while(1){
-        int cartas[5];
-        int iterador_cartas = 0;
+        // Leitura das cartas.
         for(int i = 0; i < 3; i++){
             scanf("%i", &princesa[i]);
-            cartas[iterador_cartas] = princesa[i];
-            iterador_cartas += 1;
         }
         for(int i = 0; i < 2; i++){
             scanf("%i", &principe[i]);
-            cartas[iterador_cartas] = principe[i];
-            iterador_cartas += 1;
         }
-        if(princesa[0] == 0) break;
-        // Ordena vetores.
+        if(princesa[0] == 0){
+            return 0;
+        }
+        int cartas[5] = {princesa[0], princesa[1], princesa[2], principe[0], principe[1]};
+        // Ordenação das cartas.
         if(principe[0] > principe[1]){
-            int temp = principe[0];
-            principe[0] = principe[1];
-            principe[1] = temp;
+            int temp = principe[1];
+            principe[1] = principe[0];
+            principe[0] = temp;
         }
-        insertion_sort(princesa);
-        // Compara cartas e retorna resultado.
-        printf("%i\n", compara_cartas(princesa, principe, cartas));
+        ordena_princesa(princesa);
+        // Calcula vitórias da princesa.
+        int vitorias = vitorias_princesa(princesa, principe);
+        // Calcula a menor carta necessária para o príncipe vencer.
+        printf("%i\n", calcula_resultado(vitorias, cartas, princesa[0]));
     }
 
     return 0;
